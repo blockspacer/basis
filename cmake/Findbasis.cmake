ee0946d11f9d196e1260123711808464b715930d
trofimov_d_a <derofim@y.ru>
2020-06-01 14:31:17 +0300

    REFACTOR: Find*.cmake

--- cmake/Findbasis.cmake
+++ cmake/Findbasis.cmake
@@ -7,14 +7,28 @@ list(APPEND CMAKE_MODULE_PATH ${CURRENT_CMAKE_DIR})
 # find_package(... REQUIRED)
 find_package(chromium_base REQUIRED)
 
-find_package(doctest)
-find_package(gtest)
-find_package(corrade)
-
 list(REMOVE_AT CMAKE_MODULE_PATH -1)
 
-# uses Config.cmake or a -config.cmake file
-# see https://gitlab.kitware.com/cmake/community/wikis/doc/tutorials/How-to-create-a-ProjectConfig.cmake-file
-# BELOW MUST BE EQUAL TO find_package(... CONFIG REQUIRED)
-# NOTE: find_package(CONFIG) not supported with EMSCRIPTEN, so use include()
-include(${CMAKE_CURRENT_LIST_DIR}/cmake/basis-config.cmake)
+if(NOT TARGET CONAN_PKG::basis)
+  message(FATAL_ERROR "Use basis from conan")
+endif()
+set(basis_LIB CONAN_PKG::basis)
+# conan package has '/include' dir
+set(basis_HEADER_DIR
+  ${CONAN_BASIS_ROOT}/include
+)
+if(basis_LOCAL_BUILD)
+  # name of created target
+  set(basis_LIB basis)
+  # no '/include' dir on local build
+  set(basis_HEADER_DIR
+    ${CONAN_BASIS_ROOT}
+  )
+else()
+  # uses Config.cmake or a -config.cmake file
+  # see https://gitlab.kitware.com/cmake/community/wikis/doc/tutorials/How-to-create-a-ProjectConfig.cmake-file
+  # BELOW MUST BE EQUAL TO find_package(... CONFIG REQUIRED)
+  # NOTE: find_package(CONFIG) not supported with EMSCRIPTEN, so use include()
+  include(${CMAKE_CURRENT_LIST_DIR}/cmake/basis-config.cmake)
+endif(basis_LOCAL_BUILD)
+message(STATUS "basis_HEADER_DIR=${basis_HEADER_DIR}")
